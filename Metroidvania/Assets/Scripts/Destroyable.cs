6785dca05758ee9b4160e336d862032560ff6519
CornelPL <31167551+CornelPL@users.noreply.github.com>
2019-03-20 19:04:07 +0100

    Destroyable

---
+++ Metroidvania/Assets/Scripts/Destroyable.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Destroyable : MonoBehaviour
+{
+    [SerializeField] private GameObject[] objectsToInstantiate = null;
+    [SerializeField] private int nMinObjects = 2;
+    [SerializeField] private int nMaxObjects = 5;
+    [SerializeField] private float minForce = 5f;
+    [SerializeField] private float maxForce = 15f;
+
+    void CustomDestroy()
+    {
+        // explosion particles
+
+        int nOfObjects = Random.Range(nMinObjects, nMaxObjects);
+        for (int i = 0; i < nOfObjects; i++)
+        {
+            int randomObject = Random.Range(0, objectsToInstantiate.Length);
+            GameObject objectToInstantiate = objectsToInstantiate[randomObject];
+            GameObject instantiated = Instantiate(objectToInstantiate, transform.position, Quaternion.identity);
+
+            float force = Random.Range(minForce, maxForce);
+            Vector2 direction = Random.insideUnitCircle;
+            if (direction.y < 0f) direction.y = -direction.y;
+            direction.Normalize();
+            direction = transform.TransformDirection(direction);
+
+            instantiated.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
+        }
+    }
+}

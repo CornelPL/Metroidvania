d166fc8f73bd55f07d3a0d937d240933370131b2
CornelPL <31167551+CornelPL@users.noreply.github.com>
2019-04-11 13:53:07 +0200

    Revert "Delete CustomDestroy. Use OnDestroy() instead"
    This reverts commit 7e9cd1af1dc89e2168a1286b5ec30680189dd2c9.

--- Metroidvania/Assets/Scripts/Environment/Destroyable.cs
+++ Metroidvania/Assets/Scripts/Environment/Destroyable.cs
@@ -8,7 +8,7 @@ public class Destroyable : MonoBehaviour
     [SerializeField] private float minForce = 5f;
     [SerializeField] private float maxForce = 15f;
 
-    private void OnDestroy()
+    public void CustomDestroy()
     {
         // explosion particles
 
@@ -27,6 +27,8 @@ public class Destroyable : MonoBehaviour
 
             instantiated.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
         }
+
+        Destroy(gameObject);
     }
 
     private void OnDrawGizmos()

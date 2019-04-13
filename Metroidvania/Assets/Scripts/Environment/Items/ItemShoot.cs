52bc13e58d03e8ee0f14ae519624872635595b16
CornelPL <31167551+CornelPL@users.noreply.github.com>
2019-04-13 18:44:44 +0200

    Crate added

--- Metroidvania/Assets/Scripts/Environment/Items/ItemShoot.cs
+++ Metroidvania/Assets/Scripts/Environment/Items/ItemShoot.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class ItemShoot : MonoBehaviour
 {
     public enum ItemType
     {
         rock,
+        crate,
         plank,
         barrel,
         saw
@@ -15,6 +17,8 @@ public class ItemShoot : MonoBehaviour
     [SerializeField] private Rigidbody2D rb = null;
     public bool isShooted = false;
     [SerializeField] private int plankHealth = 3;
+    [SerializeField] private List<GameObject> itemsToSpawn = null;
+    [SerializeField] private int maxItemsToSpawn = 3;
 
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -28,6 +32,10 @@ public class ItemShoot : MonoBehaviour
             }
 
             if (itemType == ItemType.rock)
+            {
+                CustomDestroy();
+            }
+            else if (itemType == ItemType.crate)
             {
                 CustomDestroy();
             }
@@ -60,6 +68,16 @@ public class ItemShoot : MonoBehaviour
 
     private void CustomDestroy()
     {
+        if (itemType == ItemType.crate)
+        {
+            int item = Random.Range(0, itemsToSpawn.Count);
+            int i = Random.Range(1, maxItemsToSpawn);
+            for(int a = 0; a < i; a++)
+            {
+                Instantiate(itemsToSpawn[item], transform.position, transform.rotation);
+            }
+        }
+
         Destroy(gameObject);
     }
 }

afc3d8ec97939c1e4b21b62d0e6505aa90532815
CornelPL <31167551+CornelPL@users.noreply.github.com>
2019-05-06 00:46:36 +0200

    Falling spikes

---
+++ Metroidvania/Assets/Scripts/Environment/FallingSpikes.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FallingSpikes : MonoBehaviour
+{
+    [SerializeField] private int damage = 20;
+    [SerializeField] private float gravityScale = 8f;
+    [SerializeField] private Rigidbody2D _rigidbody = null;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _rigidbody.gravityScale = gravityScale;
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerHealthManager>().TakeDamage(damage, transform.position.x);
+        }
+
+        Destroy(gameObject);
+    }
+}

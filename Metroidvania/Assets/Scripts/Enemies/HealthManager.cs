0b0a662f112426db92e97a3cd95debfa5f97783e
CornelPL <31167551+CornelPL@users.noreply.github.com>
2019-08-23 21:48:39 +0200

    color changing on damage moved to health manager; enemies prefabs changed to use it

--- Metroidvania/Assets/Scripts/Enemies/HealthManager.cs
+++ Metroidvania/Assets/Scripts/Enemies/HealthManager.cs
@@ -9,6 +9,10 @@ abstract public class HealthManager : MonoBehaviour
     [SerializeField] protected Transform point = null;
     [SerializeField] protected int minPoints = 0;
     [SerializeField] protected int maxPoints = 5;
+    [SerializeField] protected SpriteRenderer spriteRenderer = null;
+    [SerializeField] protected Color normalColor = Color.white;
+    [SerializeField] protected Color damageColor = Color.white;
+    [SerializeField] protected float colorChangeTime = 0.5f;
     [SerializeField] protected UnityEvent OnDeath = null;
     [SerializeField] protected UnityEvent OnTakeDamage = null;
 
@@ -16,6 +20,13 @@ abstract public class HealthManager : MonoBehaviour
 
     abstract public void TakeDamage(int damage);
     virtual public void Knockback(float xPosition, float force) { }
+
+
+    virtual public void ChangeColorOnDamage()
+    {
+        LeanTween.value( gameObject, damageColor, normalColor, colorChangeTime ).setOnUpdate( ( Color c ) => { spriteRenderer.color = c; } );
+    }
+
 
     private void Awake()
     {

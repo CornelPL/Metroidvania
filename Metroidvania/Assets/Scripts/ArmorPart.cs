9a0eb9aca0302c22e95d7122ca30c1495255c21c
CornelPL <31167551+CornelPL@users.noreply.github.com>
2019-09-18 12:42:04 +0200

    Smoke effect spawning on ground touch for armor parts

---
+++ Metroidvania/Assets/Scripts/ArmorPart.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ArmorPart : MonoBehaviour
+{
+    [SerializeField] private GameObject onFallEffect = null;
+
+    private float lastSpawnTime = 0f;
+
+
+    private void OnCollisionEnter2D( Collision2D collision )
+    {
+        if (Time.time - lastSpawnTime > 0.5f )
+        {
+            float angle = Mathf.Atan2( collision.GetContact( 0 ).normal.y, collision.GetContact( 0 ).normal.x ) * Mathf.Rad2Deg - 90f;
+
+            Instantiate( onFallEffect, collision.GetContact( 0 ).point, Quaternion.Euler( 0f, 0f, angle ) );
+
+            lastSpawnTime = Time.time;
+        }
+    }
+}

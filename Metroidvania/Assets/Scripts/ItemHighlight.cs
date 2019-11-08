edaf031fe7497529f856eef4c4633ac2bdec593d
CornelPL <31167551+CornelPL@users.noreply.github.com>
2019-11-08 20:44:41 +0100

    Better item highlight WIP

---
+++ Metroidvania/Assets/Scripts/ItemHighlight.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ItemHighlight : MonoBehaviour
+{
+    [SerializeField] private float minScale = 1f;
+    [SerializeField] private float maxScale = 1.1f;
+    [SerializeField] private float changeSpeed = 10f;
+
+    private bool wasSmall = true;
+
+
+    private void Update()
+    {
+        if ( wasSmall )
+        {
+            transform.localScale += Vector3.one * Time.deltaTime * changeSpeed;
+
+            if ( transform.localScale.x > maxScale )
+            {
+                wasSmall = false;
+            }
+        }
+        else
+        {
+            transform.localScale -= Vector3.one * Time.deltaTime * changeSpeed;
+
+            if ( transform.localScale.x < minScale )
+            {
+                wasSmall = true;
+            }
+        }
+    }
+}

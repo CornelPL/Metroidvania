7ef92fb4eb677673956ae5a1048953f8b6bb42a3
CornelPL <31167551+CornelPL@users.noreply.github.com>
2020-01-06 13:33:24 +0100

    XMasSpecial

---
+++ Metroidvania/Assets/XMasSpecial/christmastree.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class christmastree : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem treeParticles = null;
+    [SerializeField] private ParticleSystem firstExplosion = null;
+    [SerializeField] private GameObject[] balls = null;
+
+    int i = 0;
+
+    private void SpawnFirstExplosion()
+    {
+        treeParticles.Stop();
+        firstExplosion.Play();
+        for ( i = 0; i < balls.Length; i++ )
+        {
+            Invoke( "SpawnBall", i * 0.5f );
+        }
+        i = 0;
+    }
+
+    private void SpawnBall()
+    {
+        balls[ i++ ].SetActive( true );
+    }
+}
---
+++ Metroidvania/Assets/XMasSpecial/prezentItem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class prezentItem : Item
+{
+    [SerializeField] private GameObject tree = null;
+    [SerializeField] private SpriteRenderer sprite = null;
+
+    public override void Shoot( Vector2 direction, float power )
+    {
+        tree.SetActive( true );        
+        transform.SetParent( null );
+        sprite.enabled = false;
+    }
+
+
+    public override void SetFree()
+    {
+        
+    }
+
+
+    protected override void OnTriggerEnter2D( Collider2D collider )
+    {
+        if ( !isShooted )
+        {
+            return;
+        }
+
+        if ( collidersToIgnore.Find( ( Collider2D x ) => x == collider ) )
+        {
+            return;
+        }
+
+        base.OnTriggerEnter2D( collider );
+
+        CustomDestroy();
+    }
+}

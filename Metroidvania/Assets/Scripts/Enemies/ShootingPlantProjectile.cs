605801d15455f4eb81e00f49d8c479499b87a461
CornelPL <31167551+CornelPL@users.noreply.github.com>
2020-01-09 23:57:07 +0100

    shooting plant shoot effect

--- Metroidvania/Assets/Scripts/Enemies/ShootingPlantProjectile.cs
+++ Metroidvania/Assets/Scripts/Enemies/ShootingPlantProjectile.cs
@@ -5,6 +5,7 @@ public class ShootingPlantProjectile : EnemyProjectile
 {
     [SerializeField] private float shootForce = 100f;
     [SerializeField, MustBeAssigned] private Rigidbody2D _rigidbody = null;
+    [SerializeField, MustBeAssigned] private GameObject shootEffect = null;
 
     private bool isShot = false;
     private Transform player;
@@ -18,11 +19,15 @@ public class ShootingPlantProjectile : EnemyProjectile
 
     private void Update()
     {
-        if ( !isShot && _rigidbody.velocity.y <= 0f )
+        if ( !isShot && _rigidbody.velocity.y <= 0.1f )
         {
             Vector2 direction = player.position + Vector3.up * 2f - transform.position;
             _rigidbody.AddForce( direction.normalized * shootForce, ForceMode2D.Impulse );
             _rigidbody.gravityScale = 0f;
+
+            float angle = Mathf.Atan2( direction.y, direction.x ) * Mathf.Rad2Deg;
+
+            Instantiate( shootEffect, transform.position, Quaternion.AngleAxis( angle, Vector3.forward ) );
 
             isShot = true;
         }

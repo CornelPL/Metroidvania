43d4c1b5f4b376d615d1eceaf2e719a82fafc81d
CornelPL <31167551+CornelPL@users.noreply.github.com>
2020-02-16 13:20:00 +0100

    Charger walk and charge effects

---
+++ Metroidvania/Assets/Scripts/SpawnStepEffect.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SpawnStepEffect : MonoBehaviour
+{
+    [SerializeField] private Transform stepEffectSpawnPoint = null;
+    [SerializeField] private Transform stepEffectSpawnPoint2 = null;
+    [SerializeField] private GameObject[] stepEffectsBase = null;
+
+    private Queue<GameObject> stepEffects = new Queue<GameObject>();
+
+
+    public void SpawnStepEffects()
+    {
+        GameObject stepEffect = stepEffects.Dequeue();
+        stepEffect.transform.position = stepEffectSpawnPoint.position;
+        stepEffect.SetActive( true );
+        stepEffects.Enqueue( stepEffect );
+    }
+
+
+    public void Spawn2StepEffects()
+    {
+        SpawnStepEffects();
+
+        GameObject stepEffect = stepEffects.Dequeue();
+        stepEffect.transform.position = stepEffectSpawnPoint2.position;
+        stepEffect.SetActive( true );
+        stepEffects.Enqueue( stepEffect );
+    }
+
+
+    private void Start()
+    {
+        for ( int i = 0; i < stepEffectsBase.Length; i++ )
+        {
+            stepEffects.Enqueue( stepEffectsBase[ i ] );
+        }
+    }
+}

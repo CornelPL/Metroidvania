4ca1c6ae6a916d6be5ca21f39faaf71499cef5da
CornelPL <31167551+CornelPL@users.noreply.github.com>
2020-02-25 12:24:32 +0100

    Parallax fix

--- Metroidvania/Assets/Scripts/Managers/Parallax.cs
+++ Metroidvania/Assets/Scripts/Managers/Parallax.cs
@@ -31,7 +31,6 @@ public class Parallax : MonoBehaviour
     [SerializeField] private float speed = 1f;
 
     private List<Element> elements = new List<Element>();
-    private Vector3 lastCameraPosition = Vector3.zero;
 
 
     private void Awake()
@@ -43,18 +42,12 @@ public class Parallax : MonoBehaviour
     }
 
 
-    private void Start()
-    {
-        lastCameraPosition = cam.position;
-    }
-
-
     private void Update()
     {
         for ( int i = 0; i < elements.Count; i++ )
         {
             Element element = elements[ i ];
-            Vector2 distance = lastCameraPosition - cam.position;
+            Vector2 distance = element.startPos - cam.position;
             float xOffset = distance.x * element.xSpeedRatio * speed;
             float yOffset = distance.y * element.ySpeedRatio * speed;
             element.transform.position = new Vector3( element.startPos.x + xOffset, element.startPos.y + yOffset, element.startPos.z );

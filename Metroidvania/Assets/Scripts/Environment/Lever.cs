aec9bbe143efc217b34de8912f09d52538a0e04e
CornelPL <31167551+CornelPL@users.noreply.github.com>
2020-03-02 23:13:08 +0100

    Button

--- Metroidvania/Assets/Scripts/Environment/Lever.cs
+++ Metroidvania/Assets/Scripts/Environment/Lever.cs
@@ -15,6 +15,7 @@ public class Lever : MonoBehaviour
         {
             door.Open();
             opened = true;
+            GetComponent<Animator>().SetTrigger( "Death" );
         }
     }
 }

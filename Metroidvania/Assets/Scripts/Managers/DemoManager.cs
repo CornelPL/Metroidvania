c3124babc1be5f999ce16f7dceac03904cd43823
CornelPL <31167551+CornelPL@users.noreply.github.com>
2020-03-12 02:10:43 +0100

    tp to checkpoints

---
+++ Metroidvania/Assets/Scripts/Managers/DemoManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class DemoManager : MonoBehaviour
+{
+    [SerializeField] private Transform player = null;
+    [SerializeField] private Transform[] saves;
+
+    private PlayerState playerState;
+
+
+    private void Start()
+    {
+        playerState = PlayerState.instance;
+    }
+
+
+    private void Update()
+    {
+        if ( Input.GetKey( KeyCode.LeftControl ) && Input.GetKeyDown( KeyCode.Keypad1 ) )
+        {
+            player.transform.position = saves[ 0 ].position;
+            playerState.room.OnPlayerDeath();
+        }
+        else if ( Input.GetKey( KeyCode.LeftControl ) && Input.GetKeyDown( KeyCode.Keypad2 ) )
+        {
+            player.transform.position = saves[ 1 ].position;
+            playerState.room.OnPlayerDeath();
+        }
+        else if ( Input.GetKey( KeyCode.LeftControl ) && Input.GetKeyDown( KeyCode.Keypad3 ) )
+        {
+            player.transform.position = saves[ 2 ].position;
+            playerState.room.OnPlayerDeath();
+        }
+        else if ( Input.GetKey( KeyCode.LeftControl ) && Input.GetKeyDown( KeyCode.Keypad4 ) )
+        {
+            player.transform.position = saves[ 3 ].position;
+            playerState.room.OnPlayerDeath();
+        }
+    }
+}

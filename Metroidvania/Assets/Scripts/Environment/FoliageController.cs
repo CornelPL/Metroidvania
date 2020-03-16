1b1d833dd28b18f04beb6a97829a5f8297b62dfc
CornelPL <31167551+CornelPL@users.noreply.github.com>
2020-03-16 19:30:26 +0100

    Foliage anims and reaction

---
+++ Metroidvania/Assets/Scripts/Environment/FoliageController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FoliageController : MonoBehaviour
+{
+    [SerializeField] private Animator animator = null;
+    [SerializeField] private string animName = "idle";
+
+
+    private void Start()
+    {
+        animator.Play( animName, -1, Random.Range( 0f, 1f ) );
+    }
+
+
+    private void OnTriggerEnter2D( Collider2D collision )
+    {
+        animator.SetTrigger( "collision" );
+    }
+}
